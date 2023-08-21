a4360a9de5e413a5b659d47d5d2a7097a4887a6b
jyotsna-jha <116620336+jyotsna-jha@users.noreply.github.com>
2023-08-21 21:48:46 +0545

    home page1

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Lottie from "lottie-react";
 import Robot from "../Robot1.json";
-import "./HeroStyles.css";
+import "./HeroStyles.css"; // Import the CSS file
 import videobutton from "../videobutton.json";
 
 const Hero = () => {
@@ -10,13 +10,18 @@ const Hero = () => {
     <div className="hero">
       <div className="text-container">
         <h1 className="hero-text">Robotics And Automation Center</h1>
-        <p className="subtext">A team of tech loving minds</p>
+        <p className="subtext">A team of tech-loving minds</p>
       </div>
-      <div className="vdobutton">
-        <Lottie animationData={videobutton}/>
-      </div>
-      <div className="robot">
-        <Lottie animationData={Robot} />
+      <div className="content-container">
+        <div className="robot">
+          <Lottie animationData={Robot} />
+        </div>
+        <div className="vdobutton">
+          {/* Wrap Lottie component in a Link */}
+          <Link to="https://youtu.be/UMCdkk1Nf44?si=b4iPMhQXgDItMZdC" target="_blank"  >
+            <Lottie animationData={videobutton} />
+          </Link>
+        </div>
       </div>
     </div>
   );
